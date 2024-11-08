d7c421111a41560668554125a0de6ba535ed0fd6
Emilio <lunaemilio2003@gmail.com>
2024-11-08 01:08:42 -0300

    v1.0 completada

---
+++ README.md
@@ -0,0 +1,80 @@
+# <u>Email Classifier</u>
+
+## Welcome! this is the app that i made for classify your emails
+
+## I´m gonna teach to you how this work so you can modify and use to yourself
+
+### configure your email
+
+- create an application password:
+  first of all you need to say the program what is your email, for this you need to have an application password created inside of your gmail.
+
+- create a file: you will need to put your email and the app password in variables inside a file with name you want, in my case was `.env`, create it on the root of the proyect. I name the variables `smtp_username` and `smtp_password`
+
+- import the module on main.py so you can use the variables on the future, here is how i did it:
+
+```
+from dotenv import load_dotenv
+
+load_dotenv()
+
+...
+
+username = os.getenv('smtp_username')
+password = os.getenv('smtp_password')
+```
+
+### Classify your emails
+
+On main.py you will need to write the filters you need, this is what i need:
+
+```
+
+if isinstance(subject, bytes):
+try:
+subject = subject.decode(encoding if encoding and encoding.lower() != 'unknown-8bit' else 'utf-8')
+except UnicodeDecodeError:
+subject = subject.decode('latin1')
+
+            if 'password' in subject.lower():
+                folder = 'Password'
+            if 'reunion' in subject.lower():
+                folder = 'Reuniones'
+            elif 'factura' in subject.lower():
+                folder = 'Factura'
+            elif 'reclamo' in subject.lower():
+                folder = 'Reclamos'
+            elif 'LinkedIn' or 'linkedin' or 'work' or 'trabajo' in subject.lower():
+                folder = 'Trabajo'
+
+...
+
+elif content_type == 'text/plain' or content_type == 'text/html':
+try:
+body = part.get_payload(decode=True).decode('utf-8', errors='ignore')
+except UnicodeDecodeError:
+body = part.get_payload(decode=True).decode('latin1', errors='ignore')
+
+    # Here write your filters for your emails
+    if 'password' in body:
+        folder = 'Password'
+    elif 'unsubscribe' in body:
+        folder = 'Promos'
+    elif 'reunion' in body:
+        folder = 'Reuniones'
+    elif 'factura' in body:
+        folder = 'Factura'
+    elif 'reclamo' in body:
+        folder = 'Reclamos'
+    elif 'LinkedIn' or 'linkedin' or 'work' or 'trabajo' in body:
+        folder = 'Trabajo'
+
+...
+
+if 'unsubscribe' in body:
+folder = 'Promos'
+```
+
+Dont forget to create an environment to install all the dependencies with conda or `python -m venv <the name of your env>`
+
+Use `python main.py` in the proyect root to run the code, and if you want to create an executable you only have to type `pyinstaller --onefile --console --name email_classifier --icon=icon.ico main.py` on the console. You will find the `.exe` on <b>dist/email_classifier.exe</b> if you ranned the <b>pyinstaller</b> command previously.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import imaplib
 from dotenv import load_dotenv
 import os
+import email
+from email.header import decode_header
 
 load_dotenv()
 
@@ -11,22 +13,103 @@ imap_server = 'imap.gmail.com'
 username = os.getenv('smtp_username')
 password = os.getenv('smtp_password')
 
-print(f"Username: {username}")
-print(f"Password: {password}")
-
 mail = imaplib.IMAP4_SSL(imap_server, 993)
 mail.login(username, password)
 
-#load emails
-for i in mail.list()[1]:
-    l = i.decode().split(' "/" ')
-    print(f'{l[0]} = {l[1]}')
-
-#select de main view
 mail.select('"INBOX"')
 
-#look for all UNSEEN emails
-status, messages = mail.search(None, 'UNSEEN')
+status, messages = mail.search(None, 'ALL')
 
-print(messages)
-print(status)
+email_ids = messages[0].split()
+
+for index, email_id in enumerate(email_ids):
+
+    folder = None
+
+    #get the content of the mail
+    res, msg = mail.fetch(email_id, '(RFC822)')
+
+    print(f'{index + 1} de {len(email_ids)}')
+
+    for response_part in msg:
+        if isinstance(response_part, tuple):
+            message = email.message_from_bytes(response_part[1])
+
+            sender = message.get("From")
+            subject, encoding = decode_header(message.get("Subject"))[0]
+
+            date = message.get("Date")
+
+            if isinstance(subject, bytes):
+                try:
+                    subject = subject.decode(encoding if encoding and encoding.lower() != 'unknown-8bit' else 'utf-8')
+                except UnicodeDecodeError:
+                    subject = subject.decode('latin1')
+            
+            if 'password' in subject.lower():
+                folder = 'Password'
+            if 'reunion' in subject.lower():
+                folder = 'Reuniones'
+            elif 'factura' in subject.lower():
+                folder = 'Factura'
+            elif 'reclamo' in subject.lower():
+                folder = 'Reclamos'
+            elif 'LinkedIn' or 'linkedin' or 'work' or 'trabajo' in subject.lower():
+                folder = 'Trabajo'
+            
+            if message.is_multipart():
+                for part in message.walk():
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get('Content-Disposition'))
+
+                    if 'atachment' in content_disposition:
+                        filename = part.get_filename()
+                        if filename:
+                            filepath = os.path.join('adjuntos', filename)
+                            
+                            with open(filepath, 'wb') as f:
+                                f.write(part.get_payload(decode=True))
+                            print(f'adjunto guardado {filename}')
+                            
+                            if 'factura' in filename.lower():
+                                print('subiendo archivo a drive')
+
+                    elif content_type == 'text/plain' or content_type == 'text/html':
+                        try:
+                            body = part.get_payload(decode=True).decode('utf-8', errors='ignore')
+                        except UnicodeDecodeError:
+                            body = part.get_payload(decode=True).decode('latin1', errors='ignore')
+
+                        if 'password' in body:
+                            folder = 'Password'
+                        elif 'unsubscribe' in body:
+                            folder = 'Promos'
+                        elif 'reunion' in body:
+                            folder = 'Reuniones'
+                        elif 'factura' in body:
+                            folder = 'Factura'
+                        elif 'reclamo' in body:
+                            folder = 'Reclamos'
+                        elif 'LinkedIn' or 'linkedin' or 'work' or 'trabajo' in body:
+                            folder = 'Trabajo'
+            
+            else: 
+                content_type = message.get_content_type()
+                if content_type == 'text/plain' or content_type == 'text/html':
+                    try:
+                        body = message.get_payload(decode=True).decode('utf-8', errors='ignore')
+                    except (AttributeError, UnicodeDecodeError) as error:
+                        print(f'error: {error}')
+                    
+                    if 'unsubscribe' in body:
+                        folder = 'Promos'
+    if folder:
+        print('-' * 28)
+        print('archivo requerido encontrado')
+        print('-' * 28)
+        mail.create(folder)
+        mail.copy(email_id, folder)
+        mail.store(email_id, '+FLAGS', '\\Deleted')
+        mail.expunge()
+
+mail.close()
